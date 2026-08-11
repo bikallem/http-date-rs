ee26912a0bb92d6cd42cf19c5404cc4548542259
Bikal Gurung <gbikal@gmail.com>
2026-08-11 16:43:55 +0100

    init decoder

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,55 @@
 //! An HTTP date is an IMF-fixdate, e.g. `Sun, 06 Nov 1994 08:49:37 GMT`.
 //! The public API will be added here.
 
-#![warn(missing_docs)]
+pub struct DecodeError(String);
+
+impl DecodeError {
+    pub fn new(msg: impl Into<String>) -> Self {
+        Self(msg.into())
+    }
+}
+
+impl fmt::Display for DecodeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "DecodeError: {}", self.0)
+    }
+}
+
+impl std::error::Error for DecodeError {}
+
+/* ------------------- decoder ------------------- */
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum DaynNameTok {
+    Short(DaynName),
+    Long(DaynName),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum DaynName {
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
+    Sun,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum PunctuationTok {
+    Comma,
+    Space,
+}
+
+struct Decoder<'a> {
+    buf: &'a str,
+    pos: usize,
+}
+
+impl Decoder<'_> {
+    #[inline(always)]
+    fn advance(&mut self, n: usize) {
+        self.pos += n;
+    }
+}
